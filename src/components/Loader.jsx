f9e025e9398cf03f2c2fc1e5b238276853db0506
sai-myne <snm.tgi@gmail.com>
2023-10-18 14:13:33 +0800

    Similar Exercises

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Stack } from '@mui/material'
+import { InfinitySpin } from 'react-loader-spinner'
 
 const Loader = () => {
   return (
-    <div>
-      
-    </div>
+    <Stack direction='row' justifyContent='center' alignItems='center' width='100%'>
+      <InfinitySpin color='gray' />
+    </Stack>
   )
 }
 
